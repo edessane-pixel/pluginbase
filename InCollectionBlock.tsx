663278ae5f07a034a796737c51eae059c2de2fff
Elzéar DESSANE <e.dessane@student.isartdigital.com>
2026-04-19 19:37:55 +0200

    Add comprehensive implementation for InCollectionBlock.tsx

---
+++ InCollectionBlock.tsx
@@ -0,0 +1,13 @@
+// Implementation for InCollectionBlock
+import React from 'react';
+
+const InCollectionBlock = () => {
+    return (
+        <div>
+            <h2>In Collection</h2>
+            {/* Your implementation here */}
+        </div>
+    );
+};
+
+export default InCollectionBlock;
